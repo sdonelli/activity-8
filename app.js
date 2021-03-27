b835aba88caafb74c9cbfec1edbdae22676bf750
Silvia Donelli <silvia.donelli@gmail.com>
2021-03-27 19:58:09 -0300

    Exercise 1

---
+++ app.js
@@ -0,0 +1,14 @@
+/* Sum two numbers */
+var x = 1;
+var y = 2;
+var z = x + y;
+
+/* Concant two strings */
+var a = "Hello";
+var b = "World";
+var c = a.concat(b);
+
+/* Sum the lenght of two strings */
+var str1 = "Javascript";
+var str2 = "HTML";
+var result = str1.length + str2.length;
